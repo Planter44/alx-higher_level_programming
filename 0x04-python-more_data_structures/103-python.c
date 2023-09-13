5152b74e3e3069a52855def4041d48576489014f
Planter44 <planterplanter777@gmail.com>
2023-09-13 10:53:44 -0700

    cohort 4

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -8,16 +8,13 @@ void print_python_bytes(PyObject *p)
 	long int size;
 	int i;
 	char *trying_str = NULL;
-
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-
 	PyBytes_AsStringAndSize(p, &trying_str, &size);
-
 	printf("  size: %li\n", size);
 	printf("  trying string: %s\n", trying_str);
 	if (size < 10)
@@ -28,14 +25,12 @@ void print_python_bytes(PyObject *p)
 		printf(" %02hhx", trying_str[i]);
 	printf("\n");
 }
-
 void print_python_list(PyObject *p)
 {
         long int size = PyList_Size(p);
         int i;
         PyListObject *list = (PyListObject *)p;
         const char *type;
-
         printf("[*] Python list info\n");
         printf("[*] Size of the Python List = %li\n", size);
         printf("[*] Allocated = %li\n", list->allocated);
