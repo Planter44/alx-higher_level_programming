f34bd4ae32ed8146b91effd46288fb79e17d9427
Planter44 <planterplanter777@gmail.com>
2023-09-13 10:48:28 -0700

    cohort 2

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,15 +2,16 @@
 #include <Python.h>
 
 /**
- * print_python_bytes -Printing the list info.
- * @p: te object.
- * Return: Null.
+ * print_python_bytes - Prints bytes information
+ *
+ * @p: Python Object
+ * Return: no return
  */
-
 void print_python_bytes(PyObject *p)
 {
 	char *string;
 	long int size, i, limit;
+
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
@@ -20,27 +21,32 @@ void print_python_bytes(PyObject *p)
 
 	size = ((PyVarObject *)(p))->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
+
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", string);
+
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
+
 	printf("  first %ld bytes:", limit);
+
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
 			printf(" %02x", string[i]);
 		else
 			printf(" %02x", 256 + string[i]);
+
 	printf("\n");
 }
 
 /**
- * print_python_list -Printing the list info.
- * @p:object
- * Return: Null.
+ * print_python_list - Prints list information
+ *
+ * @p: Python Object
+ * Return: no return
  */
-
 void print_python_list(PyObject *p)
 {
 	long int size, i;
@@ -49,9 +55,11 @@ void print_python_list(PyObject *p)
 
 	size = ((PyVarObject *)(p))->ob_size;
 	list = (PyListObject *)p;
+
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
+
 	for (i = 0; i < size; i++)
 	{
 		obj = ((PyListObject *)p)->ob_item[i];
