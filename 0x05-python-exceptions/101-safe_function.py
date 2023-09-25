6a6232dd5a0d1a59a9deaa51c83d56c961946df4
Planter44 <planterplanter777@gmail.com>
2023-09-25 09:00:09 -0700

    cohort 21

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,16 +4,7 @@ import sys
 
 
 def safe_function(fct, *args):
-    """Executes a function safely.
-
-    Args:
-        fct: The function to execute.
-        args: Arguments for fct.
-
-    Returns:
-        If an error occurs - None.
-        Otherwise - the result of the call to fct.
-    """
+    """101-safe_function.py"""
     try:
         result = fct(*args)
         return (result)
