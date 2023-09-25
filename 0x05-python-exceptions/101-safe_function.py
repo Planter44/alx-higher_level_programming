19ae710f703fd3a8bf9271d685593952f7318926
Planter44 <planterplanter777@gmail.com>
2023-09-25 08:25:53 -0700

    cohort 17

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
-import sys
 
-
-def safe_function(fct, *args):
+def magic_calculation(a, b):
     """101-safe_function.py
     """
-    try:
-        result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return (result)
