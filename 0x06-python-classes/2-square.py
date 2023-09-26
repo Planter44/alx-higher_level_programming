cd4c6f975fd7093c7ce7adaba97d5707a7214ef4
Planter44 <planterplanter777@gmail.com>
2023-09-26 08:33:32 -0700

    Cohort 10

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Defining the class"""
+
+
 class Square:
+    """Representing square"""
+
     def __init__(self, size=0):
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
