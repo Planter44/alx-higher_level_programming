b27b95e38c73fb0c57b93702d7c8e18e5d10abcb
Planter44 <planterplanter777@gmail.com>
2023-10-09 07:57:13 -0700

    5th commit

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+MyInt function.
+"""
+
+
+class MyInt(int):
+    """Defines MyInt class"""
+
+    def __int__(self, value):
+        """MyInt class"""
+        self.value = value
+
+    def __eq__(self, other):
+        """__eq__ operator"""
+        return self.value != other
+
+    def __ne__(self, other):
+        """__ne__ operator"""
+        return self.value == other
