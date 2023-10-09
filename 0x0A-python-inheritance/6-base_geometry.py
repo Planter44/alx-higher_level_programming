aa53e91b6f4c942074739800d56f9397c0b67844
Planter44 <planterplanter777@gmail.com>
2023-10-09 06:47:13 -0700

    Second commit

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,6 +5,7 @@ The class BaseGeometry
 
 
 class BaseGeometry:
+    """Defines the BaseGeometry Class"""
     def area(self):
         """raising an exception"""
         raise Exception("area() is not implemented")
