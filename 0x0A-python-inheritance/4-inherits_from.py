e9de4e5df5c091481e26ab3ff6a15f486d803a1d
Planter44 <planterplanter777@gmail.com>
2023-10-09 06:44:05 -0700

    Second commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-The inherits_from function
+"""The
+inherits_from function.
 """
 
 
 def inherits_from(obj, a_class):
-    """True if obj is subclass of a_class"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """Returns to issubclass"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
