2e41c2ba2b1dcf853128636c34f2a283a3677654
Planter44 <planterplanter777@gmail.com>
2023-10-10 07:15:24 -0700

    11th

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,11 +7,11 @@ class Student:
     """ Class student """
 
     def __init__(self, first_name, last_name, age):
-          """ Initializing """
+        """ Initializing """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """ Returns description """
-        return self.__dict__.copy()
+        return self.__dict__
