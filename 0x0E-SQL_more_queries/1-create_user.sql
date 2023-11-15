50fa8f63a04b0d3d115c21692496aca0b966e2cf
Planter44 <planterplanter777@gmail.com>
2023-11-15 05:02:39 -0800

    More SQL

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Granting user user_0d_1priviledges.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,3 @@
+## DESCRIPTION.
+
+Alx, 0x0E-SQL_more_queries.
