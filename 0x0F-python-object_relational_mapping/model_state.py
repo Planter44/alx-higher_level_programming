1c0f23e2997c45708c7d1ba6212e211d73bd3fad
Planter44 <planterplanter777@gmail.com>
2023-12-17 13:37:56 -0800

    sql

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-The script is defining State and base classes.
+The script defines a State class.
 """
 
-import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
-    """State class"""
+    """State class
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
