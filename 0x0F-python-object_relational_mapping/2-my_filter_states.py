d23604e51941d990f155f6f85524c233806e88bd
Planter44 <planterplanter777@gmail.com>
2023-12-17 11:54:06 -0800

    Sql

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,6 +2,7 @@
 """
 The script lists all the values in states
 """
+
 import MySQLdb
 from sys import argv
 
@@ -9,6 +10,7 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
