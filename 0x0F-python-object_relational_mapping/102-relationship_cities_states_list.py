df34821bd075288a9bd636d40152d0533470ddaf
Planter44 <planterplanter777@gmail.com>
2023-12-17 13:22:43 -0800

    sql

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -14,13 +14,11 @@ if __name__ == "__main__":
     mySQL_u = sys.argv[1]
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
-
     url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
            'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
     engine = create_engine(URL(**url), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(bind=engine)
     cities = session.query(City)
-
     for city in cities:
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
