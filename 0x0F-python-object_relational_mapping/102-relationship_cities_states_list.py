b6adcf8156aeb5556da2a27b7c929b92305047fd
Planter44 <planterplanter777@gmail.com>
2023-12-17 13:00:53 -0800

    sql

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-The script lists all city objects from db.
+This the script that prints all City objects.
 """
 import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    st = session.query(State).join(City).order_by(City.id).all()
-    for state in st:
-        for city in state.cities:
-            print("{}: {} -> {}".format(city.id, city.name, state.name))
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    cities = session.query(City)
+
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
