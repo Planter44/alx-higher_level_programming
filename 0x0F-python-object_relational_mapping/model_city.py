922583a644e5f7bb2445b6d02984e55963e61aca
Planter44 <planterplanter777@gmail.com>
2023-12-17 13:57:19 -0800

    sql

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,14 +3,15 @@
 This script defines a City class
 """
 
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from relationship_state import Base
 
 class City(Base):
-    """City class
+    """class links
     """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
