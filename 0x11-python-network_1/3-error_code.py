eeb0cb0c64087e7f7474f3693b7780a0db7ad58a
Planter44 <planterplanter777@gmail.com>
2024-01-07 10:26:53 -0800

    Py Net

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """" Script that takes in a URL and Sends a request to the URL to display body of the response"""
-
-from urllib import request, error
-from sys import argv
-
 if __name__ == "__main__":
+    import urllib.error as error
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
     try:
-        with request.urlopen(argv[1]) as page:
-            print(page.read().decode('utf-8'))
+        with request.urlopen(req) as r:
+            print(r.read().decode('utf-8'))
     except error.HTTPError as e:
         print("Error code: {}".format(e.code))
